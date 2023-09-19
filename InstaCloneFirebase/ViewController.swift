a07ccce376efca526d4fd775b5381ea8856ebd25
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-19 14:24:54 -0400

    init project and initial view with firebase integration

--- InstaCloneFirebase/ViewController.swift
+++ InstaCloneFirebase/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func signUpClicked(_ sender: UIButton) {
+    }
 }
 
+#Preview {
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    let controller = storyboard.instantiateViewController(withIdentifier: "mainVC")
+    return controller
+}
